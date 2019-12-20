ae0554e7ab032d152af45a1a802b1e2c966073b3
dobrosun <mr.dobrogost02@mail.ru>
2019-12-20 15:12:02 +0300

    add recursive directory traverse

--- src/HeaderSearch.cpp
+++ src/HeaderSearch.cpp
@@ -2,19 +2,22 @@
 #include "ccli/Utility.hpp"
 
 #include <cstring>
-#include <stack>
+#include <deque>
 #include <dirent.h>
 
 
 #define GET_HEADERS_CMD "gcc -v -E -xc - < /dev/null 2>&1 | sed -ne '/starts here/,/End of/p' | grep -v '#include' | grep -v 'End of search list'"
-    
+
+
+
+
 
 static auto get_cmd_headers = [] {
     return get_splitted_exec(GET_HEADERS_CMD);
 };
 
 static auto ltrim_headers = [&](std::vector<std::string> &vec) {
-    for(unsigned int i = 0; i < vec.size(); i++) {
+    for(unsigned i = 0; i < vec.size(); i++) {
         vec[i] = ltrim(vec[i]);
     }
     return vec;
@@ -22,7 +25,7 @@ static auto ltrim_headers = [&](std::vector<std::string> &vec) {
 
 
 static auto add_I_opt = [&](std::vector<std::string> &vec) {
-    for(unsigned int i = 0; i < vec.size(); i++) {
+    for(unsigned i = 0; i < vec.size(); i++) {
         vec[i] = add_I_option(vec[i]);
     }
     return vec;
@@ -30,7 +33,7 @@ static auto add_I_opt = [&](std::vector<std::string> &vec) {
 
 static auto make_chared = [&](std::vector<std::string> &vec) {
     std::vector<const char*> res(vec.size(), nullptr);
-    for(unsigned int i = 0; i < vec.size(); i++) {
+    for(unsigned i = 0; i < vec.size(); i++) {
         res[i] = vec[i].c_str();
     }
     return res;
@@ -45,25 +48,36 @@ static auto extend = [&](std::vector<std::string> &dest, std::vector<std::string
 
 
 void put_subdirectories(std::vector<std::string> &vec, std::string &path) {
+    std::deque<std::string> deque;
+/*
     DIR *dir = opendir(path.c_str());
-    debug() << path.c_str() << " <- Opened" << std::endl;
     struct dirent *entry = readdir(dir);
+*/
+    deque.push_back(path);
 
-    debug() << "Before while loop" << std::endl;
-    while(entry) {
-        if(entry->d_type == DT_DIR) {
-			if(strcmp(entry->d_name, ".") && strcmp(entry->d_name, "..")) {
-				vec.push_back(path);
-                debug() << path << " <- pushed to subdirs" << std::endl;
+    while(!deque.empty()) {
+        std::string dir_path = deque.front();
+        deque.pop_front();
+
+        DIR *dir = opendir(dir_path.c_str());
+        struct dirent *entry = readdir(dir);
+        debug() << dir_path.c_str() << " <- Opened" << std::endl;
+        while(entry) {
+            if(entry->d_type == DT_DIR) {
+                if(strcmp(entry->d_name, ".") && strcmp(entry->d_name, "..")) {
+                    std::string subdir_path = dir_path+"/"+entry->d_name;
+
+                    vec.push_back(subdir_path);
+                    deque.push_back(subdir_path);
+
+
+                    debug() << subdir_path << " <- pushed to subdirs" << std::endl;
+                }
             }
-            //if(entry) delete entry;
-            std::string sub_path = path + entry->d_name;
-            put_subdirectories(vec, sub_path);
-
-		}
-        entry = readdir(dir);
+            entry = readdir(dir);
+        }
+        closedir(dir);
     }
-    closedir(dir);
 }
 
 
@@ -76,13 +90,13 @@ std::vector<const char*> get_headers() {
     headers = get_cmd_headers();
 
     headers = ltrim_headers(headers);
-/*
+
 	std::vector<std::string> subdirectories;
     for(std::string path: headers) {
         put_subdirectories(subdirectories, path);
     }
     extend(headers, subdirectories);
-*/
+
 
     headers = add_I_opt(headers);
 
